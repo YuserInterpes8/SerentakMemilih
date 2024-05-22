622d453016de1e0a169861f6f499c127c5181b7c
SaabiqAulawi <aulawisaabiq@gmail.com>
2024-05-22 14:13:44 +0700

    Update: Chat Admin

--- js/script.js
+++ js/script.js
@@ -25,3 +25,32 @@ fetch('footer.html')
     .then(data => {
         document.getElementById('footer').innerHTML = data;
     });
+
+fetch('chatadmin.html')
+    const chatMessages = document.querySelector('.chat-messages');
+    const chatInput = document.getElementById('chat-input');
+    const sendButton = document.getElementById('send-button');
+
+    sendButton.addEventListener('click', () => {
+        const messageText = chatInput.value.trim();
+        if (messageText !== '') {
+            const messageElement = document.createElement('div');
+            messageElement.classList.add('message', 'sent');
+            messageElement.innerHTML = `
+                <div class="message-avatar"></div>
+                <div class="message-content">
+                    <div class="message-username">Kamu</div>
+                    <p>${messageText}</p>
+                </div>`;
+            chatMessages.appendChild(messageElement);
+            chatMessages.scrollTop = chatMessages.scrollHeight;
+            chatInput.value = '';
+        }
+    });
+
+    chatInput.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+            sendButton.click();
+        }
+    });
